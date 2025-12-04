7decaccc92a37211c1841e4b42363835de6b3d0a
Nicolas <nicolaslsantos10@gmail.com>
2025-12-04 09:30:20 -0300

    adiicona items.py 2.0 dentre outros

--- POO/projeto_final/data/matematica.py
+++ POO/projeto_final/data/matematica.py
@@ -1,6 +1,4 @@
 from random import random, randrange, choice
 
-def definePeso(): 
-    return(randrange(1, 10) + (round(randrange(100, 1000)/1000, 1) *10 + choice([.0, .5]))/10.00)
-
-    
+def definePeso(base): 
+    return((randrange(1, 3) + (round(randrange(100, 1000)/1000, 1) * 10 + choice([0, .5]))/10.00))+base
